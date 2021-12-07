b3f58052aa9fefce994b0c4a8fd3870fb61fd6d5
JakeTheCorn <jakethecorn@gmail.com>
2021-12-07 13:09:40 -0500

    starting block

--- csharpcore/GildedRoseTests/ApprovalTest.cs
+++ csharpcore/GildedRoseTests/ApprovalTest.cs
@@ -12,16 +12,9 @@ namespace GildedRoseTests
     public class ApprovalTest
     {
         [Fact]
-        public void ThirtyDays()
+        public void Verify()
         {
-            var fakeoutput = new StringBuilder();
-            Console.SetOut(new StringWriter(fakeoutput));
-            Console.SetIn(new StringReader("a\n"));
-
-            Program.Main(new string[] { });
-            var output = fakeoutput.ToString();
-
-            Approvals.Verify(output);
+            // Approvals.Verify("hi....");
         }
     }
 }
