1a2f87c3b68000a712f29ef7362e1d743cff7b01
JakeTheCorn <jakethecorn@gmail.com>
2021-12-07 17:23:55 -0500

    t - add test coverage via approvals to the GuildedRose class

--- csharpcore/GildedRoseTests/ApprovalTest.cs
+++ csharpcore/GildedRoseTests/ApprovalTest.cs
@@ -1,8 +1,7 @@
-﻿using Xunit;
-using System;
-using System.IO;
-using System.Text;
+﻿using System.Collections.Generic;
 using ApprovalTests;
+using ApprovalTests.Combinations;
+using Xunit;
 using ApprovalTests.Reporters;
 using GildedRoseKata;
 
@@ -14,7 +13,49 @@ namespace GildedRoseTests
         [Fact]
         public void Verify()
         {
-            // Approvals.Verify("hi....");
+            var names = new List<string>
+            {
+                "Aged Brie",
+                "Backstage passes to a TAFKAL80ETC concert",
+                "Sulfuras, Hand of Ragnaros",
+                "Generic1",
+            };
+            var qualities = new List<int>
+            {
+                0,
+                1,
+                -1,
+                50,
+                49,
+                80
+            };
+
+            var sellIns = new List<int>
+            {
+                0,
+                6,
+                5,
+                -1,
+                10,
+                11,
+                12,
+                30,
+            };
+            CombinationApprovals.VerifyAllCombinations(DoUpdateQuality, names, qualities, sellIns);
+        }
+        
+        private string DoUpdateQuality(string name, int quality, int sellIn)
+        {
+            var item = new Item() {Name = name, Quality = quality, SellIn = sellIn};
+            var items = new List<Item>()
+            {
+                item,
+            };
+            var app = new GildedRose(items);
+                
+            app.UpdateQuality();
+
+            return item.ToString();
         }
     }
 }
